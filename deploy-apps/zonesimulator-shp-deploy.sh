535ddf310cf20f222a912c0315170412a9ad14b6
ralarcon <raul.alarcon@microsoft.com>
2024-01-24 10:30:41 +0100

    Add Sorter Simulator config

--- deploy-apps/zonesimulator-shp-deploy.sh
+++ deploy-apps/zonesimulator-shp-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Before run this script you must ensure the secrets are available"
-echo "by running secrets-creat-mqtt-passwords.sh and secrets-create-pull-serviceprincipal.sh scripts."
+echo "Before run this script you must ensure the secrets are available."
+echo "Exec secrets-creat-mqtt-passwords.sh and secrets-create-pull-serviceprincipal.sh scripts."
 echo ""
 echo "---"
 echo ""
